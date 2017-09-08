c2743005aa598bdeb064020040671166626283cc
Seifallah GUEDOUARI <seifallah.guedouari@sofrecom.com>
2017-09-08 16:38:12 +0100

    add swagger

---
+++ src/main/java/me/xstr/xstrapi/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package me.xstr.xstrapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+
+@EnableSwagger2
+
+public class SwaggerConfig {
+
+    @Bean
+    public Docket productApi() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("me.xstr.xstrapi.rest"))
+                .paths(PathSelectors.any())
+                .build();
+
+    }
+
+}
