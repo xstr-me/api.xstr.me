cdc969bcb39778e74c72ef693c03426c81c8c3c5
Seifallh Guedouari <guedouari@gmail.com>
2017-12-12 20:47:50 +0100

    allow origin *

--- src/main/java/me/xstr/xstrapi/rest/ApplicationInfoController.java
+++ src/main/java/me/xstr/xstrapi/rest/ApplicationInfoController.java
@@ -2,7 +2,8 @@ package me.xstr.xstrapi.rest;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,9 @@ import me.xstr.xstrapi.model.ApplicationInfo;
 public class ApplicationInfoController {
 	private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/info")
+    
+    @CrossOrigin(origins = "*")
+    @GetMapping(value = "/info")
     public ApplicationInfo applicationInfo(@RequestParam(value="name", defaultValue="World") String name) {
         return new ApplicationInfo(counter.incrementAndGet(),
                             String.format(TEMPLATE, name));
--- src/main/java/me/xstr/xstrapi/rest/GenericInfoController.java
+++ src/main/java/me/xstr/xstrapi/rest/GenericInfoController.java
@@ -1,7 +1,8 @@
 package me.xstr.xstrapi.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.xstr.xstrapi.model.GenericInfo;
@@ -11,10 +12,11 @@ public class GenericInfoController {
 
 	@Autowired
 	private GenericInfo genericInfo;
-	
-    @RequestMapping("/")
-    public GenericInfo genericInfo() {
-    	return genericInfo;
-    }
+
+	@CrossOrigin(origins = "*")
+	@GetMapping(value = "/")
+	public GenericInfo genericInfo() {
+		return genericInfo;
+	}
 
 }
